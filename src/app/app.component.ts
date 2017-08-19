be33a42e2889e01add943373a967128cf3772587
Max Hamulyak <max.hamulyak@icloud.com>
2017-08-19 11:40:56 +0200

    added ngrx counter example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import { INCREMENT, DECREMENT, RESET } from './counter';
+
+interface Card {
+  id: number;
+  title: string;
+  description: string;
+}
+
+interface AppState {
+  counter: number;
+}
 
 @Component({
   selector: 'MKG-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'MKG';
+
+  public cards: Card[] = [];
+
+  public counter: Observable<number>;
+
+  constructor(private store: Store<AppState>/* private http: HttpClient */) {
+    this.counter = store.select('counter');
+  }
+
+  public increment(): void {
+    this.store.dispatch({ type: INCREMENT });
+  }
+
+  public decrement(): void {
+    this.store.dispatch({ type: DECREMENT });
+  }
+
+  public reset(): void {
+    this.store.dispatch({ type: RESET });
+  }
+  ngOnInit(): void {
+    /* this.http.get<Card[]>('http://localhost:3000/api/cards').subscribe(data => {
+      console.log('data', data);
+      this.cards = data;
+    }); */
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Import HttpClientModule from @angular/common/http
+import {HttpClientModule} from '@angular/common/http';
+
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    StoreModule.forRoot({ counter: counterReducer }),
+    // Include it under 'imports' in your application module
+    // after BrowserModule.
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
